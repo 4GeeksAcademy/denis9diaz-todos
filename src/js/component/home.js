8128730470b352029a956d7b942101b984d15a08
denis9diaz <150247992+denis9diaz@users.noreply.github.com>
2024-02-15 16:13:00 +0000

    proyecto final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,31 +1,47 @@
 import React, { useState } from "react";
 
 const Home = () => {
-	const [input, setInput] = useState ([])
-	const [todos, setTodos] = useState ([])
+    const [input, setInput] = useState("");
+    const [todos, setTodos] = useState([]);
 
-	const inputKeyPress = (event) => {
-			if (event.key == "Enter") {
-			setTodos([...todos, input])
-			setInput("")
-		}
-	}
-	return (
-		<div className="list container">
-			<h2 className="title">TODOS LIST</h2>
-			<ul className="list-group">
-				<li className="list-group-item"><input onKeyDown={inputKeyPress} type="text" placeholder="What needs to be done?" value={input} onChange={(e) => {
-					setInput(e.target.value)
-				}}></input></li>
-				{todos.map((todo, index) => {
-					return (
-						<li className="list-group-item" key={index}>{todo}<button type="button" className="btn deleteButton" onClick={()=>{setTodos(todos.filter((_, i) => i !== index))}}>X</button></li>
-					)
-				})}
-				<li className="tasks list-group-item">{todos.length} tasks</li>
-			</ul>
-		</div>
-	);
+    const inputKeyPress = (event) => {
+        if (event.key === "Enter") {
+            setTodos([...todos, input]);
+            setInput("");
+        }
+    };
+
+    const deleteTodo = (index) => {
+        return () => {
+            setTodos(todos.filter((_, i) => i !== index));
+        };
+    };
+
+    return (
+        <div className="list container">
+            <h2 className="title">TODOS LIST</h2>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <input
+                        onKeyDown={inputKeyPress}
+                        type="text"
+                        placeholder="What needs to be done?"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}/>
+                </li>
+                {todos.map((todo, index) => (
+                    <li className="list-group-item" key={index}>
+                        {todo}
+                        <button
+                            type="button"
+                            className="btn deleteButton"
+                            onClick={deleteTodo(index)}>X</button>
+                    </li>
+                ))}
+                <li className="tasks list-group-item">{todos.length} tasks</li>
+            </ul>
+        </div>
+    );
 };
 
 export default Home;
